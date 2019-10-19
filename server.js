4342a32b2bfa98799d28c12681aa0a7e99771af5
Erik Navarrete <leonavas1995@gmail.com>
2019-10-19 11:51:36 +0200

    Diseño con HBS

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+app.use( express.static( __dirname + '/public' ) );
+
+
+// Express HBS engine
+hbs.registerPartials( __dirname + '/views/parciales' );
+app.set('view engine', 'hbs');
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+
+    res.render('home', {
+        nombre: 'Erik',
+        anio: new Date().getFullYear()
+    });
+
 });
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+});
+
 
 
 app.listen(3000);
